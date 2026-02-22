12008e4af2829e9c12f977823058947838cb1a87
Mews <60406199+Mews@users.noreply.github.com>
2026-02-22 23:56:15 +0000

    Implement main business logic

--- scripts/content.js
+++ scripts/content.js
@@ -1,1 +1,16 @@
-alert("hey this is working lol")
+const isDashboardPage = () => {
+    const path = window.location.pathname;
+    return path === "/" || path === "/dashboard";
+};
+
+if (isDashboardPage()) {
+    var removedElements = 0;
+
+    const copilotInputFields = document.querySelectorAll('[partial-name="copilot-chat-input-partial"]');
+    copilotInputFields.forEach( (element) => {
+        element.remove();
+        removedElements++;
+    } )
+    
+    console.log(`GitHub Unslop: Removed ${removedElements} elements.`)
+}
