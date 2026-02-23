7171618b278eb079d464544f8bde62331a6e428a
Mews <60406199+Mews@users.noreply.github.com>
2026-02-23 23:49:24 +0000

    Use settings in content script

--- scripts/content.js
+++ scripts/content.js
@@ -1,9 +1,3 @@
-// Log current settings
-(async () => {
-    const settings = await browser.storage.local.get(null)
-    console.log("Github De-slop: Current settings:", settings)
-})()
-
 const isHomePage = () => {
     const path = window.location.pathname;
     return path === "/";
@@ -13,15 +7,21 @@ const isDashboardPage = () => {
     return path === "/dashboard";
 };
 
-if (isHomePage()) {
-    const copilotInputFields = document.querySelectorAll('[partial-name="copilot-chat-input-partial"]');
-    copilotInputFields.forEach( (element) => {
-        element.remove();
-    } )
-}
-if (isDashboardPage()) {
-    const copilotInputFields = document.querySelectorAll('[partial-name="copilot-chat-input-partial"]');
-    copilotInputFields.forEach( (element) => {
-        element.remove();
-    } )
-}
+// Log current settings
+(async () => {
+    const settings = await browser.storage.local.get(null)
+    console.log("Github De-slop: Current settings:", settings)
+
+    if (isHomePage() && settings.home_page) {
+        const copilotInputFields = document.querySelectorAll('[partial-name="copilot-chat-input-partial"]');
+        copilotInputFields.forEach( (element) => {
+            element.remove();
+        } )
+    }
+    if (isDashboardPage() && settings.dashboard_page) {
+        const copilotInputFields = document.querySelectorAll('[partial-name="copilot-chat-input-partial"]');
+        copilotInputFields.forEach( (element) => {
+            element.remove();
+        } )
+    }
+})()
