8728cbe471b5ba33a2c77cb079d673a94b59bd64
Mews <60406199+Mews@users.noreply.github.com>
2026-02-23 23:26:19 +0000

    Log current settings on pages

--- scripts/content.js
+++ scripts/content.js
@@ -1,21 +1,27 @@
+// Log current settings
+(async () => {
+    const settings = await browser.storage.local.get(null)
+    console.log("Github De-slop: Current settings:", settings)
+})()
+
 const isHomePage = () => {
     const path = window.location.pathname;
     return path === "/";
 };
-
 const isDashboardPage = () => {
     const path = window.location.pathname;
     return path === "/dashboard";
 };
 
-if (isHomePage() || isDashboardPage()) {
-    var removedElements = 0;
-
+if (isHomePage()) {
     const copilotInputFields = document.querySelectorAll('[partial-name="copilot-chat-input-partial"]');
     copilotInputFields.forEach( (element) => {
         element.remove();
-        removedElements++;
     } )
-    
-    console.log(`GitHub De-slop: Removed ${removedElements} elements.`)
+}
+if (isDashboardPage()) {
+    const copilotInputFields = document.querySelectorAll('[partial-name="copilot-chat-input-partial"]');
+    copilotInputFields.forEach( (element) => {
+        element.remove();
+    } )
 }
